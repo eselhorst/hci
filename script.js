c156603f81c420029bf16e1c9f11d3d706a60e74
eselhorst <47602458+eselhorst@users.noreply.github.com>
2019-06-21 23:35:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByClassName('menutoggle')[0].onclick = function(){
-	var menu = document.getElementById('menu');
-	if(menu.className == "active"){
-		menu.className = "inactive";
-	} else {
-		menu.className = "active";
-	}
-}
